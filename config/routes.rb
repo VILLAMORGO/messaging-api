b7c237c82ba9a56872e6a014a183049e5a42ba70
VILLAMOR GO <villamorgo@gmail.com>
2022-05-03 00:16:46 +0800

    Added nested resources for conversation and messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
   scope :api, defaults: { format: :json } do
-    devise_for :users, controllers: { sessions: :sessions }, path_names: { sign_in: :login }
+    scope :v1 do
+      devise_for :users, controllers: { sessions: :sessions }, path_names: { sign_in: :login }
+      
+      resource :user, only: [:index, :show, :update]
+
+      resources :conversations do
+        resources :messages
+      end
+
+      get '/users', to: 'users#index'
+    end
   end
-
-  resource :user, only: [:index, :show, :update]
-
-  get '/users', to: 'users#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
